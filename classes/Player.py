70d1d30b355d88a288c53e1086a2a8b410763ed2
UP-4303 <up4303.pro@protonmail.com>
2022-09-15 13:04:44 +0200

    patch 2 pour Player.RequestMove()

--- classes/Player.py
+++ classes/Player.py
@@ -42,10 +42,8 @@ class Player():
     
     # Request the real player where to move
     def RequestMove(self):
-        requestedCoordinates:Vector
         print(f'Actual position : ({self.coordinates.x},{self.coordinates.y}).\n Where do you want to move ?')
-        requestedCoordinates.x = int(input('Axe X : '))
-        requestedCoordinates.y = int(input('Axe Y : '))
+        requestedCoordinates = Vector(int(input('Axe X : ')),int(input('Axe Y : ')))
         validMove, caseOccupied = self.ValidMove(requestedCoordinates)
         while not(validMove):
             if caseOccupied:
