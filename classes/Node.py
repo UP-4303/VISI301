390be8d4045a4abad08fe6bfb551596632c196a2
UP-4303 <up4303.pro@protonmail.com>
2022-09-19 15:01:00 +0200

    node modif

--- classes/Node.py
+++ classes/Node.py
@@ -3,21 +3,21 @@ from math import nan
 from classes.Position import Position
 
 class Node():
-    def __init__(self, coordinates:Position, crossable:bool, explored:bool=False, gCost:int=nan, hCost:int=nan):
+    def __init__(self, coordinates:Position, crossable:bool, start:bool=False, explored:bool=False, gCost:int=nan, hCost:int=nan):
         self.coordinates = Position()
         self.explored = explored
         self.gCost = gCost
         self.hCost = hCost
-        self.pointToNode:Position()
+        self.pointToNode:Position
+        self.start = start
 
     def Explore(self):
         self.explored = True
     
-    def Update(self, gCost:int, pointToNode:Position, start:bool=False):
+    def Update(self, gCost:int, pointToNode:Position):
         if gCost < self.gCost:
             self.gCost = gCost
         self.pointToNode = pointToNode
-        self.start = start
     
     def Cost(self):
         return self.gCost + self.hCost
--- classes/NodeList.py
+++ classes/NodeList.py
@@ -3,8 +3,8 @@ from classes.Node import Node
 from classes.Position import Position
 
 class NodeList():
-    def __init__(self, board:Board):
-        self.all = [[Node(not(type(board.get(Position(x,y))))) for x in range(board.size[0])] for y in range(board.size[1])]
+    def __init__(self, board:Board, crossableTypes:list, objectCoordinates:Position):
+        self.all = [[Node(Position(x,y), not(type(board.get(Position(x,y))) in crossableTypes), objectCoordinates == Position(x,y)) for x in range(board.size[0])] for y in range(board.size[1])]
     
     def get(self, coordinates:Position):
         return self.all[coordinates.y][coordinates.x]
