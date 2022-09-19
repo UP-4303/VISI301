3f4670da06f1e5f5de8511ca37eff391ce028b47
UP-4303 <up4303.pro@protonmail.com>
2022-09-19 16:12:27 +0200

    ajout de recherche de cible monster

--- classes/Monster.py
+++ classes/Monster.py
@@ -1,8 +1,10 @@
+from classes.Bloc import Bloc
 from classes.Board import Board
+from classes.Player import Player
 from classes.Position import Position
 from classes.Vector import Vector
 
-from utils.Pathfinder import PathfinderMonster as Pathfinder
+from utils.Pathfinder import Pathfinder
 
 class Monster():
     def __init__(self, spawnCoordinates:Position, board:Board):
@@ -11,6 +13,7 @@ class Monster():
         self.board = board
         self.healthPoints = 9
         self.movePoints = 3
+        self.uncrossableTypes = [Player, Bloc]
 
         # Treating creation on board
         if self.board.IsCaseOccupied(self.coordinates):
@@ -29,9 +32,7 @@ class Monster():
 
     # Just move the monster. USE IT CAUTIOUSLY (it can delete another object on the board)
     def Move(self):
-        moveTo = Pathfinder(self.board, self)
-        self.board.MoveObject(self.coordinates, moveTo)
-        self.coordinates.Move(moveTo - self.coordinates)
+        path = Pathfinder(self.board, self, self.Targets())
 
     # Decrease health
     def TakeDamage(self, amount:int):
@@ -40,7 +41,27 @@ class Monster():
     # Increase health
     def RecoverHealth(self, amount:int):
         self.healthPoints += amount
-    
+
+    # Find all movement targets
+    def Targets(self):
+        targets = []
+        # For every cell
+        for y in range(self.board.size[0]):
+            for x in range(self.board.size[1]):
+                # If the cell is empty
+                if type(self.board.get(Position(x,y))) == None:
+                    # Check if this cell is a valid target.
+                    # Current rule for valid target : If a cell next to the checking cell contains a player, the checking cell is valid. 
+                    if x > 0 and type(self.board.get(Position(x-1,y))) == Player:
+                        targets.append(Position(x-1,y))
+                    elif x < type(self.board.size[0]-1 and self.board.get(Position(x+1,y))) == Player:
+                        targets.append(Position(x+1,y))
+                    elif y > 0 and type(self.board.get(Position(x,y-1))) == Player:
+                        targets.append(Position(x,y-1))
+                    elif y < type(self.board.size[1]-1 and self.board.get(Position(x,y+1))) == Player:
+                        targets.append(Position(x,y+1))
+        return targets
+
     # The main code of the monster, that will be called every turn (actually just check the health)
     def PlayTurn(self):
         # Check health. If the monster is dead, it will be deleted. Else, play normally
