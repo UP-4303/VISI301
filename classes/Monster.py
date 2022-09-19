79ed18ee5e716bac350037e4cbcd118975972983
UP-4303 <up4303.pro@protonmail.com>
2022-09-19 10:03:47 +0200

    monstre pathfinder

--- classes/Monster.py
+++ classes/Monster.py
@@ -29,46 +29,9 @@ class Monster():
 
     # Just move the monster. USE IT CAUTIOUSLY (it can delete another object on the board)
     def Move(self, vector:Vector):
-        self.board.MoveObject(self.coordinates, self.coordinates + vector)
-        self.coordinates.Move(vector)
-    
-    # Check if the destination case is occupied and move only if possible
-    def CheckAndMove(self, vector:Vector):
-        if not self.board.IsCaseOccupied(self.coordinates + vector):
-            self.Move(vector)
-            didItMove = True
-        else:
-            didItMove = False
-        return didItMove
-
-    
-    # Request the real monster where to move
-    def RequestMove(self):
-        print(f'Actual position : ({self.coordinates.x},{self.coordinates.y}).\n Where do you want to move ?')
-        requestedCoordinates = Vector(int(input('Axe X : ')),int(input('Axe Y : ')))
-        validMove, caseOccupied = self.ValidMove(requestedCoordinates)
-        while not(validMove):
-            if caseOccupied:
-                print('Case is already occupied.')
-            else:
-                print(f'You only have {self.movePoints} movement points.')
-            print('Where do you want to go ? ')
-            requestedCoordinates.x = int(input('Axe X : '))
-            requestedCoordinates.y = int(input('Axe Y : '))
-            validMove, caseOccupied = self.ValidMove(requestedCoordinates)
-        print('Movement done !')
-
-
-    # Return two booleans "This move is valid" and "Case already occupied"
-    def ValidMove(self, vector:Vector):
-        if abs(vector.x) + abs(vector.y) <= self.movePoints:
-            caseOccupied = not(self.CheckAndMove(vector))
-            validMove = not(caseOccupied)
-        else:
-            caseOccupied = False
-            validMove = False
-        return validMove, caseOccupied
-        
+        moveTo = Pathfinder(self.board, self)
+        self.board.MoveObject(self.coordinates, moveTo)
+        self.coordinates.Move(moveTo - self.coordinates)
 
     # Decrease health
     def TakeDamage(self, amount:int):
@@ -84,4 +47,4 @@ class Monster():
         if self.healthPoints <= 0:
             self.DelFromBoard('MONSTER IS DEAD')
         else:
-            self.RequestMove()
+            self.Move()
