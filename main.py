7d57da26abd145c4f4419c9516a818d47b4aa62b
miladesmet <desmetmila@gmail.com>
2022-10-11 10:27:26 +0200

    Modification classes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from classes.game import Game
+from classes.Game import Game
 
 pygame.init()
 
@@ -9,34 +9,37 @@ pygame.init()
 pygame.display.set_caption("Our game")
 screen = pygame.display.set_mode((1080,720)) # define the dimention of the window
 
-#import background
-background = pygame.image.load('assets/bg.jpg')
+
 
 #download the game
-game= Game()
-
-#init player
-#player = Player()
-
-running = True #indicate  if the game is running
-
+game = Game()
 
 if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((1080, 720))
     clock = pygame.time.Clock()
-    background = pygame.image.load('assets/bg.jpg')
+    background = pygame.image.load('assets/bg.jpg') #import background
 
-    mainLoop = True
-    while mainLoop:
+    running = True #indicate  if the game is running
+
+    while running:
         screen.blit(background, (0, 0))
+
         # update screen
         pygame.display.flip()
         
         for event in pygame.event.get():
             # Quit game
             if event.type == pygame.QUIT:
-                mainLoop = False
+                running = False
+                print("Game has been closed")
+            # detect if player has touched a key on the keyboard
+            elif event.type == pygame.KEYDOWN:
+                # wich one
+                if event.key == pygame.K_q:
+                    print("you touched q")
+                elif event.key == pygame.K_RETURN:
+                    print("you touched return")
     
     pygame.quit()
     sys.exit()
