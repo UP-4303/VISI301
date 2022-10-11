1f40e09be680a976f62033cf26a047ad28e19bad
miladesmet <desmetmila@gmail.com>
2022-10-11 09:53:06 +0200

    classe personnage

---
+++ classes/character.py
@@ -0,0 +1,12 @@
+from classes.Position import Position
+from classes.MovableObject import MoveableObject
+
+class Character(MoveableObject):
+    attack:int
+    def __init__(self, name:str="Moveable", description:str="Lorem Ipsum", imageLink:str="./assets/carreblanc.png", healthPoints:int=0, position:Position=Position(0,0), movementPoints:int=0, attack:int=10):
+        super().__init__()
+        self.attack = 10
+
+
+
+
