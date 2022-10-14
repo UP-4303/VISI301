0c84b0a3c6c12324b6f352f859e59a826dca2ae2
miladesmet <desmetmila@gmail.com>
2022-10-14 23:55:06 +0200

    Factorisation en fonction

--- classes/Game.py
+++ classes/Game.py
@@ -30,49 +30,15 @@ class Game:
 
     def update(self, screen):
         # show the score on the screen
-
         font = pygame.font.SysFont("monospace", 25, True) #create the font style
         score_text = font.render("Score :" + str(self.score),1, (255,255,255))  #create texte
         screen.blit(score_text, (640,60)) #show the score at the tuple position
 
-        # show players info
-        font_small = pygame.font.SysFont("monospace", 20, True)  # create the font style
-        name_player_text = font_small.render("Name :" + str(self.player.name), 1, (255, 255, 255))  # create texte name
-        screen.blit(name_player_text, (650, 160))  # show the name at the tuple position
-        money_player_text = font_small.render("Money :" + str(self.player.money), 1, (255, 255, 255))  # create texte money
-        screen.blit(money_player_text, (650, 180))  # show the money at the tuple position
-        health_player_text = font_small.render("Health :" , 1,(255, 255, 255))  # create texte health
-        screen.blit(health_player_text, (650, 220))  # show the health at the tuple position
-
-        # Create player health bar
-        bar_back_color = (253, 250, 217)  # couleur 46, 222, 231
-        bar_back_position = [750, 230, 180, 7]  # x, y, w, h
-
-        bar_color = (148, 255, 0)  # couleur 46, 222, 231
-        bar_position = [750, 230, (self.player.healthPoints / self.player.maxHealthPoints)*180, 7]  # x, y, w, h
-
-        # Draw player helth bar
-        pygame.draw.rect(screen, bar_back_color, bar_back_position)
-        pygame.draw.rect(screen, bar_color, bar_position)
+        # show player info
+        self.draw_player_infos(screen)
 
         # show monster info
-        font_small = pygame.font.SysFont("monospace", 20, True)  # create the font style
-        name_currentMonster_text = font_small.render("Name :" + str(self.current_monster.name), 1, (255, 255, 255))  # create texte name
-        screen.blit(name_currentMonster_text, (650, 420))  # show the name at the tuple position
-
-        description_currentMonster_text = font_small.render("Description :" + str(self.current_monster.description), 1, (255, 255, 255))  # create texte description
-        screen.blit(description_currentMonster_text, (650, 440))  # show the name at the tuple position
-
-        health_currentMonster_text = font_small.render("Health :", 1, (255, 255, 255))  # create texte health
-        screen.blit(health_player_text, (650, 460))  # show the health at the tuple position
-
-        # Create player health bar
-        bar2_back_position = [750, 470, 180, 7]  # x, y, w, h
-        bar2_position = [750, 470, (self.current_monster.healthPoints / self.current_monster.maxHealthPoints) * 180, 7]  # x, y, w, h
-
-        # Draw player helth bar
-        pygame.draw.rect(screen, bar_back_color, bar2_back_position)
-        pygame.draw.rect(screen, bar_color, bar2_position)
+        self.draw_monster_infos(screen)
 
         # show monstres (maybe better in main)
         self.all_monsters.draw(screen)
@@ -87,3 +53,50 @@ class Game:
         monster = Monster()
         self.all_monsters.add(monster)
 
+    def draw_player_infos(self,screen):
+
+        # show players info
+        font_small = pygame.font.SysFont("monospace", 20, True)  # create the font style
+        name_player_text = font_small.render("Name :" + str(self.player.name), 1, (255, 255, 255))  # create texte name
+        screen.blit(name_player_text, (650, 160))  # show the name at the tuple position
+        money_player_text = font_small.render("Money :" + str(self.player.money), 1,
+                                              (255, 255, 255))  # create texte money
+        screen.blit(money_player_text, (650, 180))  # show the money at the tuple position
+        health_player_text = font_small.render("Health :", 1, (255, 255, 255))  # create texte health
+        screen.blit(health_player_text, (650, 220))  # show the health at the tuple position
+
+        # Create player health bar
+        bar_back_color = (253, 250, 217)  # couleur 46, 222, 231
+        bar_back_position = [750, 230, 180, 7]  # x, y, w, h
+
+        bar_color = (148, 255, 0)  # couleur 46, 222, 231
+        bar_position = [750, 230, (self.player.healthPoints / self.player.maxHealthPoints) * 180, 7]  # x, y, w, h
+
+        # Draw player helth bar
+        pygame.draw.rect(screen, bar_back_color, bar_back_position)
+        pygame.draw.rect(screen, bar_color, bar_position)
+
+    def draw_monster_infos(self,screen):
+        # show monster info
+        font_small = pygame.font.SysFont("monospace", 20, True)  # create the font style
+        name_currentMonster_text = font_small.render("Name :" + str(self.current_monster.name), 1,
+                                                     (255, 255, 255))  # create texte name
+        screen.blit(name_currentMonster_text, (650, 420))  # show the name at the tuple position
+
+        description_currentMonster_text = font_small.render("Description :" + str(self.current_monster.description), 1,
+                                                            (255, 255, 255))  # create texte description
+        screen.blit(description_currentMonster_text, (650, 440))  # show the name at the tuple position
+
+        health_currentMonster_text = font_small.render("Health :", 1, (255, 255, 255))  # create texte health
+        screen.blit(health_currentMonster_text, (650, 460))  # show the health at the tuple position
+
+        # Create player health bar
+        bar_back_color = (253, 250, 217)  # couleur 46, 222, 231
+        bar_color = (148, 255, 0)  # couleur 46, 222, 231
+        bar2_back_position = [750, 470, 180, 7]  # x, y, w, h
+        bar2_position = [750, 470, (self.current_monster.healthPoints / self.current_monster.maxHealthPoints) * 180,
+                         7]  # x, y, w, h
+
+        # Draw player helth bar
+        pygame.draw.rect(screen, bar_back_color, bar2_back_position)
+        pygame.draw.rect(screen, bar_color, bar2_position)
